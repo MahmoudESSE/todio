11475b3ff3d924127f866590fe9e3a28a67af4c7
Dino <dino@dinopp.site>
2024-11-13 21:12:50 +0100

    chore!: add task table and api
    Signed-off-by: Dino <dino@dinopp.site>

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -5,35 +5,29 @@ import {
   protectedProcedure,
   publicProcedure,
 } from "@/server/api/trpc";
-import { posts } from "@/server/db/schema";
+import { tasks } from "@/server/db/schema";
 
-export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+export const taskRouter = createTRPCRouter({
+  ping: publicProcedure.query(({ }) => {
+    return {
+      greeting: `PONG`,
+    };
+  }),
 
   create: protectedProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(posts).values({
+      await ctx.db.insert(tasks).values({
         name: input.name,
         createdById: ctx.session.user.id,
       });
     }),
 
-  getLatest: protectedProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.query.posts.findFirst({
-      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
+  getLatest: protectedProcedure.query(async ({ ctx: { db, session } }) => {
+    const task = await db.query.tasks.findFirst({
+      orderBy: (tasks, { desc }) => [desc(tasks.createdAt)],
     });
 
-    return post ?? null;
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
+    return task ?? null;
   }),
 });
